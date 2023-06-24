694732435cb002da3a49d2aefd3874d0a62f95f4
shashankk748 <shashank.smart748@gmail.com>
2023-06-24 21:54:55 +0530

    normal tweaks

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ ScrollReveal().reveal(".home-content p,.about-content", { origin: "right" });
 
 ////////////////////////typedd js//////////////////////////
 const typed = new Typed(".multiple-text", {
-  strings: ["Frontend Developer", "Web Enthusiast"],
+  strings: [],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
